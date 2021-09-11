b8326940e0d66be06e059d493d5f6fae64642e76
tainaracosta92@hotmail.com <tainaracosta92@hotmail.com>
2021-09-11 19:56:32 -0300

    parte lógica implementada

---
+++ script.js
@@ -0,0 +1,50 @@
+const enviar = document.getElementById("enviar")
+const imagem = document.getElementById("resolucao")
+const video = document.getElementById("video")
+
+$.ajax({
+    url: "https://api.nasa.gov/planetary/apod?api_key=NS4WpN7XODwx5ORusnsIAR94Guq1ygoWAfKc9AMe",
+    method: "get",
+    success: (resposta) => {
+        console.log(resposta)
+        document.getElementById("titulo").textContent = resposta.title
+        document.getElementById("paragrafo").textContent = resposta.explanation
+        date.setAttribute("max", resposta.date)
+        if (resposta.media_type == "image") {
+            imagem.setAttribute("src", resposta.url)
+            imagem.style.display = "flex"
+            video.style.display = "none"
+        }
+        else if (resposta.media_type == "video"){
+            video.setAttribute("src", resposta.url)
+            video.style.display = "flex"
+            imagem.style.display = "none"
+
+        }
+    }
+})
+
+enviar.addEventListener("click",() => {
+    $.ajax({
+        url:`https://api.nasa.gov/planetary/apod?api_key=NS4WpN7XODwx5ORusnsIAR94Guq1ygoWAfKc9AMe&date=${date.value}`,
+        method: "get",
+        success: (resposta) => {
+            console.log(resposta)
+            document.getElementById("titulo").textContent = resposta.title
+            document.getElementById("paragrafo").textContent = resposta.explanation
+            date.setAttribute("max", resposta.date)
+            if (resposta.media_type == "image") {
+                imagem.setAttribute("src", resposta.url)
+                imagem.style.display = "flex"
+                video.style.display = "none"
+            }
+            else if (resposta.media_type == "video"){
+                video.setAttribute("src", resposta.url)
+                video.style.display = "flex"
+                imagem.style.display = "none"
+    
+            }
+        }
+    })
+}
+ )
